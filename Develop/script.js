9574735ff87b88536fb99b96b719509a7fa2a2ab
Carmen Regina <crcarmen23@gmail.com>
2023-09-21 10:16:42 -0500

    added variables and started confirm and prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,29 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", 
+"O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", 
+"o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var special = ["#","@","!","$","%","^","&","*","?","+","<",">"]
+var number = [0,1,2,3,4,5,6,7,8,9]
+
+function generatePassword () {
+prompt("How many characters would you like your password to have?")
+
+confirm("Click OK to confirm the use of uppercase letters in your password.")
+
+confirm("Click OK to confirm the use of lowercase letters in your password.")
+
+confirm("Click OK to confirm the use of special characters in your password.")
+
+confirm("Click OK to confirm the use of numbers in your password.")
+
+return "string"
+}
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
